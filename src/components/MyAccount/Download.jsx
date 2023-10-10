17742b37c790517d789b868cf168aac149261450
Raj Chavda <raj.theappideas@gmail.com>
2023-10-10 12:02:01 +0530

    add pagination to download & orders

--- src/components/MyAccount/Download.jsx
+++ src/components/MyAccount/Download.jsx
@@ -3,11 +3,29 @@ import { useSelector } from "react-redux";
 import BaseUrl from "../../BaseUrl";
 import { useTranslation } from "react-i18next";
 import moment from "moment";
+import { useState } from "react";
+import ReactPaginate from "react-paginate";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
 const Download = () => {
+  const [pageNumber, setPageNumber] = useState(0);
+
   const { downloads, loading } = useSelector((state) => state.root.cart);
 
   const { t } = useTranslation();
+
+  // pagination logic
+  const downloadsPerPage = 8;
+  const pageVisited = pageNumber * downloadsPerPage;
+  let displayDownloads = downloads.slice(
+    pageVisited,
+    downloadsPerPage + pageVisited
+  );
+  const pageCount = Math.ceil(downloads.length / downloadsPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   return (
     <>
@@ -24,7 +42,7 @@ const Download = () => {
                   <th className="md:p-4 p-2 text-center">{t("Action")}</th>
                 </thead>
                 <tbody>
-                  {downloads.map((magazine) => (
+                  {displayDownloads.map((magazine) => (
                     <tr key={magazine?._id}>
                       <td className="md:p-4 p-3 font-medium text-left whitespace-nowrap flex md:flex-row flex-col items-center justify-start gap-3">
                         <img
@@ -55,6 +73,37 @@ const Download = () => {
                   ))}
                 </tbody>
               </table>
+              <hr />
+              {/* pagination */}
+              <div className="flex xl:flex-row flex-col items-center w-full gap-3">
+                <div className="w-full bg-white p-3 flex md:flex-row flex-col gap-3 items-center justify-between">
+                  <ReactPaginate
+                    onPageChange={changePage}
+                    previousLabel={
+                      <p className="bg-gray-200 w-10 h-10 p-2 rounded-md">
+                        <BsChevronLeft className="h-5 w-5 rounded-md text-black" />
+                      </p>
+                    }
+                    nextLabel={
+                      <p className="bg-gray-200 w-10 h-10 p-2 rounded-md">
+                        <BsChevronRight className="h-5 w-5 rounded-md text-black" />
+                      </p>
+                    }
+                    pageClassName="bg-gray-200 text-black px-2 py-2 rounded-md text-center"
+                    pageLinkClassName="p-2"
+                    breakLabel="..."
+                    breakClassName=""
+                    breakLinkClassName=""
+                    pageRangeDisplayed={3}
+                    marginPagesDisplayed={1}
+                    pageCount={pageCount}
+                    containerClassName=""
+                    activeClassName="active"
+                    className="flex items-center md:gap-3 gap-2 flex-wrap"
+                    forcePage={pageNumber}
+                  />
+                </div>
+              </div>
             </div>
           ) : (
             <div className="w-full loading">{t("No Downloads here.")}</div>
--- src/components/MyAccount/Orders.jsx
+++ src/components/MyAccount/Orders.jsx
@@ -4,15 +4,28 @@ import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import moment from "moment";
 import { handleFindSingleOrder } from "../../redux/CartSlice";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import ReactPaginate from "react-paginate";
 
 const Orders = () => {
   const [showOrderDetails, setShowOrderDetails] = useState(false);
+  const [pageNumber, setPageNumber] = useState(0);
 
   const { orders, loading } = useSelector((state) => state.root.cart);
 
   const dispatch = useDispatch();
 
   const { t } = useTranslation();
+
+  // pagination logic
+  const ordersPerPage = 8;
+  const pageVisited = pageNumber * ordersPerPage;
+  let displayOrders = orders.slice(pageVisited, ordersPerPage + pageVisited);
+  const pageCount = Math.ceil(orders.length / ordersPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   return (
     <>
@@ -38,7 +51,7 @@ const Orders = () => {
                       <th className="md:p-4 p-2 text-center">{t("Action")}</th>
                     </thead>
                     <tbody>
-                      {orders.map((order) => (
+                      {displayOrders.map((order) => (
                         <tr key={order?._id}>
                           <td className="md:p-4 p-3 font-medium text-center whitespace-nowrap">
                             #{order?.orderId}
@@ -72,6 +85,37 @@ const Orders = () => {
                       ))}
                     </tbody>
                   </table>
+                  <hr />
+                  {/* pagination */}
+                  <div className="flex xl:flex-row flex-col items-center w-full gap-3">
+                    <div className="w-full bg-white p-3 flex md:flex-row flex-col gap-3 items-center justify-between">
+                      <ReactPaginate
+                        onPageChange={changePage}
+                        previousLabel={
+                          <p className="bg-gray-200 w-10 h-10 p-2 rounded-md">
+                            <BsChevronLeft className="h-5 w-5 rounded-md text-black" />
+                          </p>
+                        }
+                        nextLabel={
+                          <p className="bg-gray-200 w-10 h-10 p-2 rounded-md">
+                            <BsChevronRight className="h-5 w-5 rounded-md text-black" />
+                          </p>
+                        }
+                        pageClassName="bg-gray-200 text-black px-2 py-2 rounded-md text-center"
+                        pageLinkClassName="p-2"
+                        breakLabel="..."
+                        breakClassName=""
+                        breakLinkClassName=""
+                        pageRangeDisplayed={3}
+                        marginPagesDisplayed={1}
+                        pageCount={pageCount}
+                        containerClassName=""
+                        activeClassName="active"
+                        className="flex items-center md:gap-3 gap-2 flex-wrap"
+                        forcePage={pageNumber}
+                      />
+                    </div>
+                  </div>
                 </div>
               ) : (
                 <div className="w-full loading">{t("No Orders here.")}</div>

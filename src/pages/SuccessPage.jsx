7041bea6586b5d9c7c52abed9ee06eb3789c7f66
Raj Chavda <raj.theappideas@gmail.com>
2023-10-19 14:21:54 +0530

    miner change

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Success from "../components/Success";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import BaseUrl from "../BaseUrl";
 import toast from "react-hot-toast";
 import { useEffect } from "react";
@@ -13,27 +13,29 @@ const SuccessPage = () => {
 
   const query = useLocation().search;
   const token = query.split("?")[1].split("=")[1];
+  const navigate = useNavigate();
 
   const handleVerifyToken = async () => {
-    if (token) {
-      setLoading(true);
-      try {
-        const { data } = await axios("/api/verify-token", {
-          method: "post",
-          data: {
-            token,
-          },
-        });
-        setMessage(data?.message);
-        setLoading(false);
-      } catch (error) {
-        toast.error(error.message);
-        setLoading(false);
-      }
+    setLoading(true);
+    try {
+      const { data } = await axios("/api/verify-token", {
+        method: "post",
+        data: {
+          token,
+        },
+      });
+      setMessage(data?.message);
+      setLoading(false);
+    } catch (error) {
+      toast.error(error.message);
+      setLoading(false);
     }
   };
 
   useEffect(() => {
+    if (!token) {
+      navigate("/");
+    }
     handleVerifyToken();
   }, []);
 

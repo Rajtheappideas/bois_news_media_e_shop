a4ae4166cf95ceb5408bc7402d61571e2c6fa0ae
unknown <alexandreriehl20@gmail.com>
2023-12-12 13:46:57 +0100

    Update URL to target backend

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const language = JSON.parse(window.localStorage.getItem("lang"));
 
 // export default axios.defaults.baseURL = "http://15.235.192.7:3000";
-export default axios.defaults.baseURL = "https://boisnewsmedia.onrender.com";
+export default axios.defaults.baseURL = "https://api.boisnewsmedia.com";
 
 export const PostUrl = axios.create({
-  baseURL: "https://boisnewsmedia.onrender.com/api/user",
+  baseURL: "https://api.boisnewsmedia.com/api/user",
   method: "POST",
   headers: {
     "Accept-Language": language,
@@ -14,7 +14,7 @@ export const PostUrl = axios.create({
 });
 
 export const GetUrl = axios.create({
-  baseURL: "https://boisnewsmedia.onrender.com/api/user",
+  baseURL: "https://api.boisnewsmedia.com/api/user",
   method: "GET",
   headers: {
     "Accept-Language": language,
